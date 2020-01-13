b01c41a822603e35d3043ac0a2ad1dd7358584a5
dariansaric <darian.saric@fer.hr>
2020-01-13 20:08:30 +0100

    Ispravljen format ispisa vremena, netestiran update

--- socket-server.js
+++ socket-server.js
@@ -34,11 +34,26 @@ const game = { // ovo je izgled utakmice
     }
 }; // todo: pametniji način smisljanja utakmica
 const games = [];
+const connections = [];
 standardInput.setEncoding('utf-8');
 standardInput.on('data', data => {
     console.log('primio sam: %s', data);
     if (data === 'exit\n') {
+        console.log("WARNING: prekidam rad servera...");
         process.exit();
+    }
+
+    // vrste updatea: rezultat, vrijeme
+    let input = data.split("/-(.+)/");
+    switch (input[0]) {
+        case "TIME": // mijenjaj vrijeme
+            update_time(JSON.parse(input[1]));
+            break;
+        case "SCORE": // mijenjaj rezultat
+            update_score(JSON.parse(input[1]));
+            break;
+        default: // ne valja input
+            console.log("WARNING: neispravna update naredba '%s'", input);
     }
 });
 init_data();
@@ -50,6 +65,7 @@ const wss = new WebSocketServer({"port": 8081});
 wss.on('connection', (ws, req) => {
     const client_remote_address = req.connection.remoteAddress;
     console.log("LOG: spojen novi klijent s '%s'! Šaljem sve utakmice...", client_remote_address);
+    connections.push(ws);
     /*
     format poruka:
     {vrsta poruke: sve, dodaj jednu (opt), makni jednu (opt), update
@@ -67,4 +83,39 @@ wss.on('connection', (ws, req) => {
 
 function init_data() {
     games.push(game);
+}
+
+function update_time(update) {
+    if (update.id === null) {
+        // error log
+        return;
+    } else if (update.time === null) {
+        // error log
+        return;
+    }
+
+    let game = retrieve_game_for_id(update.id);
+    if (game === null) {
+        //error log
+        return;
+    }
+    game.time = update.time;
+    // broadcast?
+    broadcast_game_update(game);
+}
+
+function retrieve_game_for_id(id) {
+    for (let g in games) {
+        if (g.id === id) {
+            return g;
+        }
+    }
+
+    return null;
+}
+
+function broadcast_game_update(game) {
+    let msg = {type: "update"};
+    msg.game = game;
+    connections.forEach(ws => ws.send(msg))
 }
